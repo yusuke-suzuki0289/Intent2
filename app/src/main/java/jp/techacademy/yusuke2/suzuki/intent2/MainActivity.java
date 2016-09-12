7c36bfa06927b7053371ba6959741a059a3f6b8b
yusuke-suzuki0289 <yusu.suz.0320@gmail.com>
2016-09-12 16:28:35 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/yusuke2/suzuki/intent2/MainActivity.java
@@ -0,0 +1,32 @@
+package jp.techacademy.yusuke2.suzuki.intent2;
+
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText mEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mEditText = (EditText) findViewById(R.id.editText);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, mEditText.getText().toString());
+                startActivity(intent);
+            }
+        });
+    }
+}
